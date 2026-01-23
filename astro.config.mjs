3a40f82408f8f0feae7c11e35e36553c679128a8
えーぷる <keita023@icloud.com>
2026-01-23 23:17:26 +0900

    Fix local build; ensure assets served; prep for CF Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "static",
   trailingSlash: "always",
   integrations: [tailwind()],
-  adapter: cloudflare({
-    mode: "static"
-  })
 });
